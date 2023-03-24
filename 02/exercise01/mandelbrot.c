b22407eca5c0552eb2e492585a1b7a85c53a596f
csba2165 <daniel.rufinatscha@student.uibk.ac.at>
2023-03-24 13:01:28 +0100

    exercise1

--- 02/exercise01/mandelbrot.c
+++ 02/exercise01/mandelbrot.c
@@ -12,8 +12,22 @@
 #define Y 720
 #define MAX_ITER 10000
 
-void calc_mandelbrot(uint8_t image[Y][X]) {
-	// TODO: Write your code here!
+void calc_mandelbrot(uint8_t image[Y][X]){
+	for(int i = 0; i < Y; i++){
+		for(int j = 0; j < X; j++){
+			double x0 = (double)j / X * 3.5 - 2.5;
+			double y0 = (double)i / Y * 2 - 1;
+			double x = 0, y = 0;
+			int iter = 0;
+			while(x*x + y*y <= 2*2 && iter < MAX_ITER){
+				double xtemp = x*x - y*y + x0;
+				y = 2*x*y + y0;
+				x = xtemp;
+				iter++;
+			}
+			image[i][j] = iter % 256;
+		}
+	 }
 }
 
 int main() {
