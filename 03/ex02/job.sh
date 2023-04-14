e84461f7ec79a75b4740e3fc28db03fec811792f
csba2165 <daniel.rufinatscha@student.uibk.ac.at>
2023-04-14 14:32:20 +0200

    dsadasd

--- 03/ex02/job.sh
+++ 03/ex02/job.sh
@@ -13,18 +13,14 @@
 # Enforce exclusive node allocation, do not share with other jobs
 #SBATCH --exclusive
 
-OMP_NUM_THREADS=1 printf "1 Thread: "
-#./ex2 1500
-for i in {1..10}; do ./ex2 1500; done
+OMP_NUM_THREADS=1 printf "1 Thread: \n"
+for i in {1..10}; do ./ex2 1500 printf "\n"; done
 
-OMP_NUM_THREADS=2 printf "\n2 Threads: "
-#./ex2 1500
-for i in {1..10}; do ./ex2 1500; done
+OMP_NUM_THREADS=2 printf "\n2 Threads: \n"
+for i in {1..10}; do ./ex2 1500 printf "\n"; done
 
-OMP_NUM_THREADS=4 printf "\n4 Threads: "
-#./ex2 1500
-for i in {1..10}; do ./ex2 1500; done
+OMP_NUM_THREADS=4 printf "\n4 Threads: \n"
+for i in {1..10}; do ./ex2 1500 printf "\n"; done
 
-OMP_NUM_THREADS=8 printf "\n8 Threads: "
-#./ex2 1500
-for i in {1..10}; do ./ex2 1500; done
+OMP_NUM_THREADS=8 printf "\n8 Threads: \n"
+for i in {1..10}; do ./ex2 1500 printf "\n"; done
