2e9c8069e597ecc5b3529c5bca694ae10a6ca5e5
csba2165 <daniel.rufinatscha@student.uibk.ac.at>
2023-04-14 14:34:52 +0200

    dsadqwds

--- 03/ex02/job.sh
+++ 03/ex02/job.sh
@@ -14,13 +14,13 @@
 #SBATCH --exclusive
 
 OMP_NUM_THREADS=1 printf "1 Thread: \n"
-for i in {1..10}; do ./ex2 1500 printf "\n"; done
+for i in {1..10}; do ./ex2 1500; printf "\n"; done
 
 OMP_NUM_THREADS=2 printf "\n2 Threads: \n"
-for i in {1..10}; do ./ex2 1500 printf "\n"; done
+for i in {1..10}; do ./ex2 1500; printf "\n"; done
 
 OMP_NUM_THREADS=4 printf "\n4 Threads: \n"
-for i in {1..10}; do ./ex2 1500 printf "\n"; done
+for i in {1..10}; do ./ex2 1500; printf "\n"; done
 
 OMP_NUM_THREADS=8 printf "\n8 Threads: \n"
-for i in {1..10}; do ./ex2 1500 printf "\n"; done
+for i in {1..10}; do ./ex2 1500; printf "\n"; done
