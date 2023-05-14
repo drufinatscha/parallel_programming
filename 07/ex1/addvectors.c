ad12ee1c015723ed9bcf8976c6064dbf4b412b2e
csba2165 <daniel.rufinatscha@student.uibk.ac.at>
2023-05-14 23:32:33 +0200

    add all old exercises

---
+++ 07/ex1/addvectors.c
@@ -0,0 +1,40 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<omp.h>
+
+
+#define SIZE 100000
+#define REPETITIONS 1000000
+
+int main() {
+    float a[SIZE], b[SIZE], c[SIZE];
+    
+    // Initialize the vectors
+    for (int i = 0; i < SIZE; ++i) {
+        a[i] = 0.0f;
+        b[i] = 1.0f;
+        c[i] = 2.0f;
+    }
+
+    // Perform the computation and measure the execution time
+    double start_time = omp_get_wtime();
+
+    for (int run = 0; run < REPETITIONS; ++run) {
+        for (int i = 0; i < SIZE; ++i) {
+            a[i] += b[i] * c[i];
+        }
+    }
+
+    double end_time = omp_get_wtime();
+    printf("Programm duration: %2.4f\n", end_time - start_time);
+
+    // Output the results
+    for (int i = 0; i < SIZE; ++i) {
+        if(a[i]!=2000000.0f) {
+            printf("%2.4f ",a[i]);
+        }
+    }
+
+    
+    return EXIT_SUCCESS;
+}
